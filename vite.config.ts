b47ddf6bfc89102772c797abc4040854c05dc0c5
Marc-Antoine Lacasse <m.a.l.musique@gmail.com>
2026-05-05 14:08:52 -0400

    fix: fix frontend asset path (css/frontend)

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/js/frontend/app.ts',
-                'resources/frondend/app.scss',
+                'resources/css/frondend/app.scss',
                 'resources/css/app.css',
                 'resources/js/app.tsx'
             ],
